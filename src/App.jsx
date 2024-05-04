dafbc2ae783c0304cfcd8de3678fe87bbbb9954b
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2024-05-04 23:32:34 +0500

    latest commit 04/05/24-11:30

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Components/GlobalComponents/Header"
+import BannerSection from "./Components/Home/BannerSection"
+
+
+function App() {
+  
+  return (
+    <>
+     <Header/>
+     <BannerSection/>
+    </>
+  )
+}
+
+export default App
